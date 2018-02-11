e41fdd2aa63b8ac3846f4685a7f8e8f310d93d59
Andrew Bass <andybass009@gmail.com>
2018-02-11 03:11:05 -0500

    Add utility to create users table

--- db/util.py
+++ db/util.py
@@ -3,10 +3,34 @@ import MySQLdb as sql
 
 import db
 
+TABLES = {}
+
+TABLES["users"] = """
+    CREATE TABLE users (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        first_name varchar(256) NOT NULL,
+        last_name varchar(256) NOT NULL,
+
+        email varchar(256) NOT NULL,
+        password varchar(256) NOT NULL,
+
+        PRIMARY KEY(id)
+    );
+"""
+
 def init():
     """
     Creates tables
     """
     conn = db.conn()
+    cursor = conn.cursor()
 
-    print(conn) 
+    for name, cmd in TABLES.items():
+        try:
+            drop_cmd = "DROP TABLE IF EXISTS {};".format(name)
+
+            cursor.execute(drop_cmd)
+            cursor.execute(cmd)
+        except sql.Error as err:
+            print(err)
+
