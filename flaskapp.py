cd20dec1723f102af352929ab5bad964abb021b9
Andrew Bass <andybass009@gmail.com>
2018-02-11 02:00:05 -0500

    Cleanup

--- flaskapp.py
+++ flaskapp.py
@@ -1,25 +1,19 @@
 from flask import Flask, render_template, request
-import configparser 
 
 import subprocess
+
 import users.routes
+
+import db.util
+import db.config
 
 app = Flask(__name__)
 app.register_blueprint(users.routes.blueprint, url_prefix='/user')
-
-
-#Databse ini parse
-parser = configparser.ConfigParser()
-parser.sections()
-parser.read('../database.ini')
 
 @app.route('/')
 @app.route('/<name>')
 def hello_world(name=None):
 	return render_template('hello.html', name=name)
-
-
-
 
 @app.route('/update/git', methods = ['GET', 'POST'])
 def update():
@@ -28,17 +22,14 @@ def update():
 
 @app.route('/database/connectiontest')
 def connectiokn_test():
+        config = db.config.read()
+        output = "User: " + config['database']['user'] + "<br>Host: " + config['database']['host'] 
         
-        output = "User: " + parser['database']['user'] + "<br>Host: " + parser['database']['host'] 
         return output
 
-
-
-
-
-
-
-
+@app.cli.command("db-init")
+def db_init():
+    db.util.init()
 
 if __name__ == '__main__':
   app.run(debug=True)
