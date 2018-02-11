ab0d738eb9083d019223dc54d4f391a33e1f7ad0
Andrew Bass <andybass009@gmail.com>
2018-02-11 17:11:26 -0500

    Seperate database errors into seperate file

--- users/routes.py
+++ users/routes.py
@@ -3,9 +3,8 @@ from flask import request, Blueprint
 
 import auth
 import auth.token
-import users.db
 
-import json
+import users.db
 
 blueprint = Blueprint('users', __name__)
 
@@ -15,24 +14,25 @@ def login(user):
 
     try:
         user_id = users.db.check_creds(user, passwd)
-    except users.db.CredError as err:
+    except db.Error as err:
         return err.message
 
     return auth.token.create(user_id)
     
-@blueprint.route('/logout/<user>', methods = ['GET'])
+@blueprint.route('/logout', methods = ['GET'])
 @auth.required
-def logout(user):
+def logout():
     return "Hello there: {}".format(auth.user())
 
 @blueprint.route('/create/<user>', methods = ['POST'])
 def create(user):
     pass
 
-@blueprint.route('/delete/<user>', methods = ['POST'])
-def delete(user):
-    pass
+@blueprint.route('/delete', methods = ['POST'])
+@auth.required
+def delete():
+    delete_user(auth.user())
 
-@blueprint.route('/change-password/<user>', methods = ['POST'])
-def change_password(user):
+@blueprint.route('/change-password', methods = ['POST'])
+def change_password():
     pass
