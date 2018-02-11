558fc0a9368ba5eb18ad3fd11dc5b5729d2dbd3b
Andrew Bass <andybass009@gmail.com>
2018-02-11 03:33:30 -0500

    Add util command for making users

--- db/util.py
+++ db/util.py
@@ -26,11 +26,26 @@ def init():
     cursor = conn.cursor()
 
     for name, cmd in TABLES.items():
-        try:
-            drop_cmd = "DROP TABLE IF EXISTS {};".format(name)
+        drop_cmd = "DROP TABLE IF EXISTS {};".format(name)
 
+        try:
             cursor.execute(drop_cmd)
             cursor.execute(cmd)
         except sql.Error as err:
             print(err)
 
+def add_user(first, last, email, passwd):
+    conn = db.conn()
+    cursor = conn.cursor()
+
+    cmd = """
+        INSERT INTO users (first_name, last_name, email, password)
+        VALUES ('{}', '{}', '{}', '{}');
+    """.format(first, last, email, passwd)
+
+    try:
+        cursor.execute(cmd)
+    except sql.Error as err:
+        print(err)
+
+    conn.commit()
--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import click
 
 import subprocess
 
@@ -27,9 +28,25 @@ def connectiokn_test():
         
         return output
 
+# Command Line Utils
+# NOT ACCESSIBLE BY WEB, DONT WORRY
+# Use like so on terminal: `flask $CMD_NAME_HERE $ARG1 $ARG2 ... $ARGN
+
 @app.cli.command("init-db")
-def db_init():
-    db.util.init()
+def db_init(*args, **kwargs):
+    db.util.init(*args, **kwargs)
+
+@app.cli.command("init-db")
+def db_init(*args, **kwargs):
+    db.util.init(*args, **kwargs)
+
+@app.cli.command("add-user")
+@click.option("--first", default="FIRST")
+@click.option("--last", default="LAST")
+@click.option("--email", default="EMAIL")
+@click.option("--passwd", default="PASSWD")
+def db_add_user(first, last, email, passwd):
+    db.util.add_user(first, last, email, passwd)
 
 if __name__ == '__main__':
   app.run(debug=True)
