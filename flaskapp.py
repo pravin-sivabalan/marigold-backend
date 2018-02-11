7ee29b9beb7fb8124bb4e00dc7bcfeae5cb18aa4
Will Borland <wborland@purdue.edu>
2018-02-10 23:24:56 -0500

    Added database ini file to server and created example in flaskapp.py for how to parse ini file

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request
-import subprocess
+import configparser 
 
+import subprocess
 import users.routes
 
 app = Flask(__name__)
-
 app.register_blueprint(users.routes.blueprint, url_prefix='/users')
+
+
+#Databse ini parse
+parser = configparser.ConfigParser()
+parser.sections()
+parser.read('../database.ini')
+
 
 @app.route('/')
 @app.route('/<name>')
@@ -19,6 +26,20 @@ def hello_world(name=None):
 def update():
 	cmd = ["./hook.sh", ]
 	return "hi"
+
+@app.route('/database/connectiontest')
+def connectiokn_test():
+        
+        output = "User: " + parser['database']['user'] + "<br>Host: " + parser['database']['host'] 
+        return output
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
