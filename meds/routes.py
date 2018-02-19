5fafb7facf2cc14decaa8f54f90bfb0c74f7eb44
Andrew Bass <andybass009@gmail.com>
2018-02-19 12:15:53 -0500

    Add medication deletion

--- meds/routes.py
+++ meds/routes.py
@@ -28,3 +28,16 @@ def add():
 def for_user():
     users_meds = meds.db.for_user()
     return jsonify(message="ok", meds=users_meds)
+
+@blueprint.route('/delete', methods = ['POST'])
+@auth.required
+def delete():
+    data = request.get_json()
+
+    try:
+        med_id = data["id"]
+    except KeyError as err:
+        raise MissingDataError(err)
+
+    meds.db.delete(med_id)
+    return jsonify(message="ok")
