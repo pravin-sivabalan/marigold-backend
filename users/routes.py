a5f4c231ac0cab079fa5e50eb7f4dfbea918657b
Andrew Bass <andybass009@gmail.com>
2018-02-19 18:47:51 -0500

    Add messge ok

--- users/routes.py
+++ users/routes.py
@@ -32,9 +32,10 @@ def register():
     password = data.get('password')
 
     user_id = users.db.create_user(first_name, last_name, email, password)
-    return jsonify({
-        "jwt": auth.token.create(user_id).decode("utf-8")
-    })
+    return jsonify(
+        message="ok",
+        jwt=auth.token.create(user_id).decode("utf-8")
+    )
 
 @blueprint.route('/logout', methods = ['GET'])
 @auth.required
