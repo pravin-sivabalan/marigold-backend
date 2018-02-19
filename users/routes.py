dd8181e4976aaddb1f2774da37bc43f05c5cb731
Will Borland <wborland@purdue.edu>
2018-02-19 16:54:46 -0500

    Initial email setup

--- users/routes.py
+++ users/routes.py
@@ -6,6 +6,9 @@ import auth.token
 
 import users.db
 
+import boto3
+from botocore.exceptions import ClientError
+
 blueprint = Blueprint('users', __name__)
 
 @blueprint.route('/login/<user>', methods = ['POST'])
@@ -33,6 +36,67 @@ def delete():
     users.db.delete_user(auth.uid())
     return "Goodbye!"
 
-@blueprint.route('/change-password', methods = ['POST'])
-def change_password():
-    pass
+
+@blueprint.route('/change-password/<email>')
+def change_password(email):
+
+    SENDER = "Marigold <mailer@marigoldapp.net>"
+    RECIPIENT = email;
+    AWS_REGION = "us-east-1";
+
+    SUBJECT = "Amazon SES Test (SDK for Python)"
+
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+             "This email was sent with Amazon SES using the "
+             "AWS SDK for Python (Boto)."
+            )
+
+    test = "Hello world"
+
+
+    BODY_HTML = """<html><head></head><body><h1>Amazon SES Test (SDK for Python) """ + test + """</h1><p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+        AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>"""
+
+
+    CHARSET = "UTF-8"
+
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    try:
+        response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                RECIPIENT,
+            ],
+        },
+        Message={
+            'Body': {
+                'Html': {
+                    'Charset': CHARSET,
+                    'Data': BODY_HTML,
+                },
+                'Text': {
+                    'Charset': CHARSET,
+                    'Data': BODY_TEXT,
+                },
+            },
+            'Subject': {
+                'Charset': CHARSET,
+                'Data': SUBJECT,
+            },
+        },
+        Source=SENDER,
+    )
+
+    except ClientError as e:
+        return e.response['Error']['Message'];
+    else:
+        return "Success! Email sent to " + email;
+
+
+
+
