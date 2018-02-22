7d1030441a569d9ee4b7a6dba214f1ab9d678943
Andrew Bass <andybass009@gmail.com>
2018-02-22 16:50:57 -0500

    Add tests for logging in, registering, and getting user profile

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,40 @@
 
-import unittest as ut
 from tests import BaseTestCase
 
-class UserTestCase(BaseTestCase):
-    def test_f(self):
-        self.assertEqual(1, 1)
+import unittest as ut
+import json
 
-if __name__ == "__main__":
-    ut.main()
+class UserTestCase(BaseTestCase):
+    def test_register(self):
+        rv = self.post('/user/register', dict(
+            first_name="Test",
+            last_name="User",
+            email="abc@abc.com",
+            password="123"
+        ))
+
+        self.assertEqual(rv.status_code, 200)
+        
+        data = json.loads(rv.data)
+        self.assertEqual(data["message"], "ok")
+
+    def test_login(self):
+        self.test_register()
+        self.login(email="abc@abc.com", password="123")
+
+        self.assertIsNotNone(self.jwt)
+
+    def test_profile(self):
+        self.test_register()
+        self.login(email="abc@abc.com", password="123")
+
+        rv = self.auth_get('/user')
+        self.assertEqual(rv.status_code, 200)
+
+        data = json.loads(rv.data)
+        profile = data["profile"]
+
+        self.assertEqual(profile["first_name"], "Test") 
+        self.assertEqual(profile["last_name"], "User") 
+
+        self.assertEqual(profile["email"], "abc@abc.com")
