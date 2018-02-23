a3f620d9901d3aa1b9600137657914013a31f3a1
Andrew Bass <andybass009@gmail.com>
2018-02-22 21:36:43 -0500

    Add more base code for getting fake user data

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,13 @@ import flaskapp
 import db.util
 
 import unittest as ut
+
 import json
+import random
+import string
+
+def rand_str(size):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
 class BaseTestCase(ut.TestCase):
     def setUp(self):
@@ -16,8 +22,13 @@ class BaseTestCase(ut.TestCase):
         with flaskapp.app.app_context() as ctx:
             db.util.init()
 
-    def post(self, route, data):
-        return self.app.post(route, data=json.dumps(data), content_type='application/json')
+    def post(self, route, data, headers=None):
+        if headers is not None:
+            return self.app.post(route, data=json.dumps(data), 
+                                 content_type='application/json', headers=headers)
+        else:
+            return self.app.post(route, data=json.dumps(data), content_type='application/json')
+
 
     def make_auth_headers(self):
         return dict(
@@ -26,6 +37,9 @@ class BaseTestCase(ut.TestCase):
 
     def auth_get(self, route):
         return self.app.get(route, headers=self.make_auth_headers())
+
+    def auth_post(self, route):
+        return self.post(route, headers=self.make_auth_headers())
 
     def login(self, email, password):
         rv = self.post('/user/login', dict(
@@ -40,5 +54,18 @@ class BaseTestCase(ut.TestCase):
 
         self.jwt = data["jwt"]
 
+    def fake_user(self):
+        rv = self.post('/user/register', dict(
+            first_name=rand_str(10),
+            last_name=rand_str(10),
+            email = "{}@{}.com".format(rand_str(5), rand_str(5)),
+            password = "{}@{}.com".format(rand_str(5), rand_str(5))
+        ))
+
+        self.assertEqual(rv.status_code, 200)
+        data = json.loads(rv.data)
+
+        self.jwt = data["jwt"]
+        return rv
 if __name__ == "__main__":
     ut.main()
