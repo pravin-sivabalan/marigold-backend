d6eb6e6ba8ae009b30c35b3dbf2fe7c7493ff73d
Will Borland <wborland@purdue.edu>
2018-02-25 22:48:05 -0500

    Delete email addition

--- users/email.py
+++ users/email.py
@@ -10,18 +10,13 @@ import string
 
 
 def change_password(email):
-
-
     random_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(10)])
     address = "https://marigoldapp.net/user/update-password/" + random_string;
-
 
     SENDER = "Marigold <mailer@marigoldapp.net>"
     RECIPIENT = email;
     AWS_REGION = "us-east-1";
-
     SUBJECT = "MariGold Password Reset"
-
     BODY_TEXT = ("Amazon SES Test (Python)\r\n"
              "This email was sent with Amazon SES using the "
              "AWS SDK for Python (Boto)."
@@ -29,19 +24,13 @@ def change_password(email):
 
     test = "Hello world";
     user_id = users.db.find_user_email(email)
-
-
     BODY_HTML = """<html><head><img height="100" src="https://s3.amazonaws.com/marigoldapp/MariGoldLogo.png"></head>
     <body><h2 style='font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif'>You have requested to reset your password for MariGold.</h2>
         <p style='font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif'>Please click <a href='""" + address + """'>here</a> or go to """ + address + """ to reset your password.</p>
     </body>
     </html>"""
-
-
     CHARSET = "UTF-8"
-
     client = boto3.client('ses',region_name=AWS_REGION)
-
     try:
         response = client.send_email(
         Destination={
@@ -67,9 +56,6 @@ def change_password(email):
         },
         Source=SENDER,
     )
-
-
-
     except ClientError as e:
         return e.response['Error']['Message'];
     else:
@@ -87,3 +73,56 @@ def update_password(link, password):
     print("Updated")
 
     return 1
+
+
+def delete_account_email(email):
+
+
+    SENDER = "Marigold <mailer@marigoldapp.net>"
+    RECIPIENT = email;
+    AWS_REGION = "us-east-1";
+    SUBJECT = "MariGold Accout Deletion"
+    BODY_TEXT = ("You have deleted your Marigodl account.\r\n"
+            
+            )
+
+
+    BODY_HTML = """<html><head><img height="100" src="https://s3.amazonaws.com/marigoldapp/MariGoldLogo.png"></head>
+    <body><h2 style='font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif'>You have deleted your MariGold account.</h2>
+        <p style='font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif'>If this was a mistake please reply to this email.</p>
+    </body>
+    </html>"""
+    CHARSET = "UTF-8"
+    client = boto3.client('ses',region_name=AWS_REGION)
+    try:
+        response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                RECIPIENT,
+            ],
+        },
+        Message={
+            'Body': {
+                'Html': {
+                    'Charset': CHARSET,
+                    'Data': BODY_HTML,
+                },
+                'Text': {
+                    'Charset': CHARSET,
+                    'Data': BODY_TEXT,
+                },
+            },
+            'Subject': {
+                'Charset': CHARSET,
+                'Data': SUBJECT,
+            },
+        },
+        Source=SENDER,
+    )
+    except ClientError as e:
+        return e.response['Error']['Message'];
+    else:
+        return jsonify(message="ok")
+
+
+
