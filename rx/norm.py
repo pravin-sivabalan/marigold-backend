1d80c16aec9d39fe3c6f972c7e0b09ce2716abe0
Andrew Bass <andybass009@gmail.com>
2018-03-15 19:10:31 -0400

    Add lookup by name

--- rx/norm.py
+++ rx/norm.py
@@ -9,6 +9,21 @@ from error import Error
 class MultipleIdsError(Error):
     """Multiple RxCuis exist for a given NDC"""
 
+def lookup_name(name):
+    """
+    Returns the id associated with a drug given its's name
+    """
+    data = rx.get('rxcui.json', params=dict(name=name)).json()
+    ids = data.get('idGroup').get('rxnormId')
+
+    if ids == None:
+        return None
+
+    if len(ids) > 1:
+        raise MultipleIdsError()
+
+    return ids[0]
+
 def lookup_ndc(ndc):
     """
     Returns the id associated with a drug given its's NDC code
@@ -23,3 +38,15 @@ def lookup_ndc(ndc):
         raise MultipleIdsError()
 
     return ids[0]
+
+def lookup_approx(term):
+    pass
+
+def terms(cui):
+    """
+    Gets associated RxTerms for a given CUI, including name, strength, and more
+    """
+    path = "/RxTerms/rxcui.json/{}/allinfo".format(cui)
+
+    data = rx.get(path)
+    print(data.text)
