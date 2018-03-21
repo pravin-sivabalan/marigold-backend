b0381a4acfc4b238cfb056538c13d1ca71c32b98
Andrew Bass <andybass009@gmail.com>
2018-03-21 12:53:36 -0400

    Basic lookup working

--- meds/lookup.py
+++ meds/lookup.py
@@ -4,12 +4,31 @@ from error import Error
 
 import rx.norm
 
+accepted_routes = ["oral", "chewable", "tablet", "capsule"]
+
 class CouldNotFindBaseIngredient(Error):
     """The given name could not be resolved to a base drug"""
     status_code = 400
     error_code = 901
 
-def perform(name):
+def is_mouth_drug(drug):
+    for route in accepted_routes:
+        if route in drug.name.lower() or route in drug.synonym.lower():
+            return True
+
+    return False
+
+def contains_name(actual_name, drug):
+    name = actual_name.lower()
+    return name in drug.name.lower() or name in drug.synonym.lower()
+
+def jsonify(match, name_field):
+    return dict(
+        name = getattr(match, name_field),
+        cui = match.cui
+    )
+
+def perform(name, limit=10):
     candidates = rx.norm.lookup_approx(name)
 
     if len(candidates) == 0 or candidates[0].score < 75:
@@ -25,4 +44,12 @@ def perform(name):
     desired_type = "SBD" if tty == "BN" else "SCD"
 
     drugs = rx.norm.related_by_types(cui, types=[desired_type])
-    print(drugs)
+    matches = [drug for drug in drugs 
+                if is_mouth_drug(drug) and contains_name(actual_name, drug)]
+
+    matches.sort(key=lambda drug: drug.name)
+
+    if tty == "BN":
+        return [jsonify(match, name_field="synonym") for match in matches]
+    else:
+        return [jsonify(match, name_field="name") for match in matches]
