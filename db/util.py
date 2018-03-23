2e4eeff9884b8d5e6a748208cef3335e68026fa6
Andrew Bass <andybass009@gmail.com>
2018-03-23 11:51:27 -0400

    Fix database init code for testing

--- db/util.py
+++ db/util.py
@@ -8,52 +8,49 @@ import auth
 tables = {}
 
 tables["users"] = """
-    CREATE TABLE {} (
-        id int(11) NOT NULL AUTO_INCREMENT,
-
-        first_name varchar(256) NOT NULL,
-        last_name varchar(256) NOT NULL,
-
-        email varchar(256) NOT NULL,
-        password varchar(256) NOT NULL,
-
-        league varchar(256),
-
-        PRIMARY KEY(id)
-    );
+    CREATE TABLE `{}` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `first_name` varchar(256) NOT NULL,
+        `last_name` varchar(256) NOT NULL,
+        `email` varchar(256) NOT NULL,
+        `password` varchar(256) NOT NULL,
+        `league` varchar(256) DEFAULT NULL,
+        `user_medication_ids` mediumtext,
+        `banned_substance_ids` mediumtext,
+        `allergies` mediumtext,
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 """
 
 tables["user_meds"] = """
-    CREATE TABLE {} (
-        id int NOT NULL AUTO_INCREMENT,
-
-        name mediumtext NOT NULL,
-        dose int NOT NULL,
-        quantity int NOT NULL,
-
-        run_out_date date NOT NULL, 
-
-        uid int NOT NULL,
-        mid int,
-
-        rxcui varchar(256),
-       
-        PRIMARY KEY(id)
-    );
+    CREATE TABLE `user_meds` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `user_id` int(11) NOT NULL,
+        `medication_id` int(11) DEFAULT NULL,
+        `name` mediumtext NOT NULL,
+        `quantity` int(11) DEFAULT NULL,
+        `run_out_date` date DEFAULT NULL,
+        `temporary` int(11) DEFAULT NULL,
+        `rxcui` mediumtext,
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 """
 
 tables["meds"] = """
-    CREATE TABLE {} (
-        id int NOT NULL AUTO_INCREMENT,
-
-        name mediumtext NOT NULL,
-        dose int(11) NOT NULL,
-        expir_date date NOT NULL,
-
-        uid int(11) NOT NULL,
-       
-        PRIMARY KEY(id)
-    );
+    CREATE TABLE `meds` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `substance_name` mediumtext,
+        `adverse_reaction` mediumtext,
+        `application_number` mediumtext,
+        `boxed_warning` mediumtext,
+        `brand_name` mediumtext,
+        `drug_interactions` mediumtext,
+        `generic_name` mediumtext,
+        `information_for_patients` mediumtext,
+        `product_type` mediumtext,
+        `route` mediumtext,
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 """
 
 def init():
