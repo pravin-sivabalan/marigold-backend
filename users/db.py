7269a6e5d6711d265d770a20598827ceb2ebd525
Will Borland <wborland@purdue.edu>
2018-04-18 13:14:52 -0400

    Got ride of refill

--- users/db.py
+++ users/db.py
@@ -135,7 +135,7 @@ def find_user(uid, custom_query=find_user_with_id):
 
 def user_profile(uid):
     return find_user(uid, """
-        SELECT first_name, last_name, email, league, allergies, pharmacy_name, pharmacy_address, pharmacy_phone, refill FROM users
+        SELECT first_name, last_name, email, league, allergies, pharmacy_name, pharmacy_address, pharmacy_phone FROM users
         WHERE id = %s
     """)
 
